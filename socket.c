3791342b621f8b6e79a63e7df54e4c52eba86a0f
Drasungor <aguscambiano@gmail.com>
2019-08-31 12:10:55 -0300

    Function firms added

--- socket.c
+++ socket.c
@@ -1,1 +1,22 @@
 #include "socket.h"
+
+
+
+
+
+
+
+
+void socket_init(socket_t *socket){
+}
+
+void socket_release(socket_t *socket){
+}
+
+int socket_bind_and_listen(socket_t *socket, const char *service){
+  return 0;
+}
+
+int socket_connect(socket_t *socket, const char *host, const char *service){
+  return 0;
+}
--- socket.h
+++ socket.h
@@ -1,3 +1,20 @@
 #ifndef _SOCKET_H_
 #define  _SOCKET_H_
+
+
+typedef struct{
+
+}socket_t;
+
+
+void socket_init(socket_t *socket);
+
+void socket_release(socket_t *socket);
+
+int socket_bind_and_listen(socket_t *socket, const char *service);
+
+int socket_connect(socket_t *socket, const char *host, const char *service);
+
+
+
 #endif
