f96ed1c293d9a8b389efc9df0160149a2d0bab8a
Drasungor <aguscambiano@gmail.com>
2019-09-09 15:34:37 -0300

    Fixed line length "errors"

--- sudoku_handler.h
+++ sudoku_handler.h
@@ -14,13 +14,18 @@ int sudoku_handler_init(sudoku_handler_t *sudoku_handler);
 
 void sudoku_handler_release(sudoku_handler_t *sudoku_handler);
 
-int sudoku_handler_set_number(sudoku_handler_t *sudoku_handler, int number, int vertical_position, int horizontal_position);
+int sudoku_handler_set_number(sudoku_handler_t *sudoku_handler,
+                              int number,
+                              int vertical_position,
+                              int horizontal_position);
 
 void sudoku_handler_reset(sudoku_handler_t *sudoku_handler);
 
 bool sudoku_handler_verify(sudoku_handler_t *sudoku_handler);
 
-void sudoku_handler_get_board(sudoku_handler_t *sudoku_handler, char buffer[VERTICAL_DIM_PRINTED_BOARD][HORIZONTAL_DIM_PRINTED_BOARD + 1]);
+void sudoku_handler_get_board(sudoku_handler_t *sudoku_handler,
+                              char buffer[VERTICAL_DIM_PRINTED_BOARD]
+                                         [HORIZONTAL_DIM_PRINTED_BOARD + 1]);
 
 
 
