362c19eb31e2eac0acfc36c1e70f8980baf46df8
Drasungor <aguscambiano@gmail.com>
2019-09-09 19:20:05 -0300

    Added _ prefix to private functions

--- sudoku_handler.c
+++ sudoku_handler.c
@@ -11,15 +11,14 @@
 #define FILE_ERROR -3
 #define MEMORY_ERROR -4
 
-static int char_to_int(char c){
+static int _char_to_int(char c){
   return c - 48;
 }
 
 
-
 //This function can't be reduced to 15 lines due to variable
 //initialization and error checking
-static int board_file_to_matrix(FILE *file,
+static int _board_file_to_matrix(FILE *file,
                                 int matrix[BOARD_DIMENSION][BOARD_DIMENSION]){
   char *line;
   size_t size;
@@ -35,7 +34,7 @@ static int board_file_to_matrix(FILE *file,
     }
     for (size_t j = 0; j < BOARD_DIMENSION; j++) {
       number = line[2*j];
-      matrix[i][j] = char_to_int(number);
+      matrix[i][j] = _char_to_int(number);
     }
     free(line);
   }
@@ -52,30 +51,12 @@ int sudoku_handler_init(sudoku_handler_t *sudoku_handler){
   }
 
   int board [BOARD_DIMENSION][BOARD_DIMENSION];
-  int program_state = board_file_to_matrix(board_file, board);
+  int program_state = _board_file_to_matrix(board_file, board);
 
   fclose(board_file);
   if (program_state != SUCCESS) {
     return program_state;
   }
-  //MODULARIZAR MUCHO
-  /*
-  for (size_t i = 0; i < BOARD_DIMENSION; i++) {
-    line = NULL;
-    size = 0;
-    size = getline(&line, &size, sudoku_file);
-    if (size == -1) {
-      free(line);
-      return MEMORY_ERROR;
-    }
-    for (size_t j = 0; j < BOARD_DIMENSION; j++) {
-      number = line[2*j];
-      board[i][j] = char_to_int(number);
-    }
-    free(line);
-  }
-  */
-  //fclose(sudoku_file);
   sudoku_init(&(sudoku_handler->sudoku), board);
   return SUCCESS;
 }
