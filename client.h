1d54d49bdec5047cea37caf88d769f2e9b74e183
Drasungor <aguscambiano@gmail.com>
2019-09-09 22:21:02 -0300

    Documented .h functions

--- client.h
+++ client.h
@@ -10,14 +10,15 @@ typedef struct{
   socket_t sckt;
 }client_t;
 
-
+//Initializes the client's elements
+//Returns 0 if it's successful, -1 if there is a socket error
 int client_init(client_t *client, const char *host, const char *service);
 
 void client_release(client_t *client);
 
 //Executes all the commands specified in stdin, killing the process
 //when EOF or the exit are found. If there is a memory or socket error
-//the process is also killed but it returns -1. otherwise returns 0
+//the process is also killed and returns 1. otherwise returns 0
 int client_operate(client_t *client);
 
 #endif
